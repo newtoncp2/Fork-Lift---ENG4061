db03e23d7176e1a53202a1136f7dc35cdea50dab
louisgaming <siqueiraluizlb@gmail.com>
2026-07-02 16:23:47 -0300

    ajusta arctan

--- python/raspberry_pi/apriltag.py
+++ python/raspberry_pi/apriltag.py
@@ -163,7 +163,7 @@ def _vision_worker():
                                 cont = 0
 
                                 rho_lin = np.sqrt(x0**2 + z_lin**2)
-                                theta_lin = np.arctan2(x0, z_lin)
+                                theta_lin = np.arctan2(z_lin, x0)
                                 theta_k = np.arctan2(kz, kx)    
                                 theta_ef = theta_k - theta_lin 
                                 theta_volta = -(np.pi/2 - theta_k)
