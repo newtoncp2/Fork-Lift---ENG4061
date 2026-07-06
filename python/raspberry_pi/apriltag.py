3a129c2856649cc1adaed2e7c22f339de8df547d
louisgaming <siqueiraluizlb@gmail.com>
2026-07-06 16:38:11 -0300

    ajuste

--- python/raspberry_pi/apriltag.py
+++ python/raspberry_pi/apriltag.py
@@ -213,6 +213,7 @@ def _vision_worker():
                         estado = "confirmar"
                         
                         if etapa_aprox > 2:
+                            estado = "ideal"
                             etapa_aprox = 0 
                     case "ideal":
                         comando = ideal[etapa_ideal]
