396ba97d7c190893736a036d5afe9bfad5639c92
Gustans <gugab04@gmail.com>
2026-07-06 20:38:30 -0300

    recalibrate

--- python/camera_calibration/tiraimagens.py
+++ python/camera_calibration/tiraimagens.py
@@ -1,7 +1,11 @@
 import cv2
 
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(-1, cv2.CAP_V4L2)  # Use V4L2 backend for Raspberry Pi
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320) # lower resolution for faster processing
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1) # Set buffer size to 1 to reduce latency
+cap.set(cv2.CAP_PROP_FPS, 15)
 ind = 0
 
 while ind < 30:  
