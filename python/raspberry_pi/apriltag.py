1785ddeceff73c50b0d803670d56ad539d613ee8
louisgaming <siqueiraluizlb@gmail.com>
2026-07-07 14:06:42 -0300

    ajustes

--- python/raspberry_pi/apriltag.py
+++ python/raspberry_pi/apriltag.py
@@ -234,7 +234,7 @@ def _vision_worker():
                                         z_lin = z0 - 0.2 / 2 if z0 > 0 else z0 + 0.2 / 2
 
                                         theta_lin = angulo_entre_rad(n_cam_tag_space, [x0,0,z_lin])
-                                        theta_volta = -angulo_entre_rad([0,0,-1], [x0,0,z_lin])
+                                        theta_volta = -angulo_entre_rad([0,0,-1], n_cam_tag_space)
                                         
                                         rho_lin = np.sqrt(x0**2 + z_lin**2)*0.9
                                         
@@ -242,6 +242,7 @@ def _vision_worker():
                                             theta_volta = np.pi/2 - theta_volta
                                         
                                         print(f"x0: {x0}, z0': {z0}")
+                                        print(n_cam_tag_space)
                                         print(f"theta_lin: {theta_lin}, theta_volta: {theta_volta}") 
                                         aprox = [f"1 {theta_lin}",f"2 {abs(rho_lin)}", f"1 {theta_volta}"] 
 
