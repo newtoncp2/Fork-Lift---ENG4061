a17da25bddba40e27d6a82a0d54ae93582fe9b7f
louisgaming <siqueiraluizlb@gmail.com>
2026-07-06 21:54:53 -0300

    teste

--- python/raspberry_pi/apriltag.py
+++ python/raspberry_pi/apriltag.py
@@ -191,7 +191,7 @@ def _vision_worker():
                                         if theta_ef > 0 :    
                                             theta_volta = -np.cos(z_lin/rho_lin) - np.pi/4
                                         else:
-                                            theta_volta = -np.sin(z_lin/rho_lin) - np.pi/4
+                                            theta_volta = np.sin(z_lin/rho_lin) - np.pi/4
                                         print(f"x0: {x0}, rho': {rho_lin}")
                                         print(f"theta_ef: {theta_ef}, theta_volta: {theta_volta}") 
                                         aprox = [f"1 {theta_ef}",f"2 {rho_lin}", f"1 {theta_volta}"] 
