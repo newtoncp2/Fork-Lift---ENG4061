f38897264c669fc8faeb59f7e10b7c65dfcd3a2e
louisgaming <siqueiraluizlb@gmail.com>
2026-07-08 12:40:00 -0300

    aumenta resolução

--- python/raspberry_pi/setup.py
+++ python/raspberry_pi/setup.py
@@ -134,8 +134,8 @@ def setup_resources(base_dir: str | None = None):
     try:
         import cv2
         cap = cv2.VideoCapture(-1, cv2.CAP_V4L2)  # Use V4L2 backend for Raspberry Pi
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320) # lower resolution for faster processing
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640) # lower resolution for faster processing
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         cap.set(cv2.CAP_PROP_BUFFERSIZE, 1) # Set buffer size to 1 to reduce latency
         cap.set(cv2.CAP_PROP_FPS, 15)
         if not cap.isOpened():
