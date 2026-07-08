150759c56d7aa754de5904fb303e49e440c46979
louisgaming <siqueiraluizlb@gmail.com>
2026-07-08 17:20:07 -0300

    ajuste

--- python/raspberry_pi/apriltag.py
+++ python/raspberry_pi/apriltag.py
@@ -235,11 +235,11 @@ def _vision_worker():
                                         z0 = -posicao_camera[2] # SE Z0 CHEGA COMO NEGATIVO, z0 = -posicao_camera[2]
 
                                         '''robo para 0.15 m à frente da câmera'''
-                                        z_lin =  posicao_camera[2] - 0.35  
+                                        z_lin =  posicao_camera[2] - 0.15  
 
-                                        rho_lin = np.sqrt(x0**2 + z_lin**2)
+                                        rho_lin = (x0**2 + z_lin**2)**0.5
 
-                                        w = np.array([0.0, 0.0, 0.35]) - np.array(posicao_camera)
+                                        w = np.array([0.0, 0.0, 0.15]) - np.array(posicao_camera)
                                         
                                         theta_lin = angulo_entre_rad(n_cam_tag_space, w)
                                         theta_volta = angulo_entre_rad([0,0,1], w)
